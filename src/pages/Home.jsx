cb20dca0e30c694e835a5d510d614404d1501456
Sanjievkumar <murugansanjievkumar@gmail.com>
2026-06-24 18:59:29 +0530

    UI: Remove Insights, QuoteBanner, and ProductsSlider from Home page as requested

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,10 @@ import Hero from '../components/Hero';
 import BrandsSection from '../components/BrandsSection';
 import FeaturesSection from '../components/FeaturesSection';
 import StatsBanner from '../components/StatsBanner';
-import ProductsSlider from '../components/ProductsSlider';
 import FeaturedProducts from '../components/FeaturedProducts';
 import IndustriesGrid from '../components/IndustriesGrid';
 import CoreValues from '../components/CoreValues';
-import QuoteBanner from '../components/QuoteBanner';
 import CtaSection from '../components/CtaSection';
-import InsightsSection from '../components/InsightsSection';
 
 const Home = () => {
   return (
@@ -21,17 +18,11 @@ const Home = () => {
 
       <FeaturedProducts />
       
-      <QuoteBanner />
-
-      <ProductsSlider />
-      
       <FeaturesSection />
 
       <IndustriesGrid />
       
       <StatsBanner />
-      
-      <InsightsSection />
       
       <CtaSection />
     </main>
